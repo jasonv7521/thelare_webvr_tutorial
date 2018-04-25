4efdb7ee0ec4f63596eb8035edd788d7afd50fe8
jasonv7521 <38737185+jasonv7521@users.noreply.github.com>
2018-04-25 17:05:19 -0400

    Update theLareCommonCode.js

--- public/theLareCommonCode.js
+++ public/theLareCommonCode.js
@@ -4,7 +4,7 @@ var vRViewPlayer = function(elementIdName, options) {
 }
 
 var drawingRoom = vRViewPlayer('#first-vr-example', {
-  image: 'https://raw.githubusercontent.com/lare-student/thelare_webvr_tutorial/master/public/assets/underwater-vr.jpg',
+  image: 'thelare_webvr_tutorial/public/theLareCommonCode.js',
   is_stereo: false,
 });
 
